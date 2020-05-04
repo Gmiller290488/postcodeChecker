bfe4c7cf09ed43d5840acbe47c5dd46c55bcc252
Gareth Miller <gmiller290488@gmail.com>
2020-05-04 16:20:03 +0100

    added register form - which works

--- postCodeBE/postcodeApp/views.py
+++ postCodeBE/postcodeApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import UserForm
 
-# Create your views here.
+def register_user(request):
+    form = UserForm(request.POST)
+    if form.is_valid():
+        user = form.save(commit=True)
+        user.save()
+    return render(request, 'postcodeApp/register_user.html', {'form': form})
