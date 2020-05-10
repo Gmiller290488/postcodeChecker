c74905d3a84a56e8f15250ff083c859a432165d8
Gareth Miller <gmiller290488@gmail.com>
2020-05-10 13:19:07 +0100

    refactored cron.py

--- postCodeBE/postcodeApp/cron.py
+++ postCodeBE/postcodeApp/cron.py
@@ -1,51 +1,8 @@
-import requests, os, bs4, chromedriver_binary, time, smtplib, yagmail, keyring
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests, os, bs4, time, smtplib, yagmail, keyring, json
 from .models import User 
 
-browser = webdriver.Firefox()
 emailValue = "gmiller290488@gmail.com"
 winnerPostcode = ""
-
-
-def loadBrowser():
-    url = "https://pickmypostcode.com/account/"
-    browser.maximize_window()
-    browser.get(url)
-
-def enterDetails():
-    postcodeValue = "n44eb"
-    postcodeElemValue = "confirm-ticket"
-    postcodeElem = browser.find_element_by_id(postcodeElemValue)
-    postcodeElem.click()
-    postcodeElem.send_keys(postcodeValue)
-
-    emailElemValue = "confirm-email"
-    emailElem = browser.find_element_by_id(emailElemValue)
-    emailElem.click()
-    emailElem.send_keys(emailValue)
-
-def goToHomePage():
-    signinXpath = "/html/body/div[2]/div[3]/main/div/div[1]/ul/li[1]/div/div/section/div/form/button"
-    signinButtonElem = browser.find_element_by_xpath(signinXpath)
-    signinButtonElem.click()
-
-    mainPageXpath = "/html/body/div[2]/header/div/h1/a/img[2]"
-    mainpageElem = browser.find_element_by_xpath(mainPageXpath)
-    mainpageElem.click()
-
-def acceptCookies():
-    cookiesXpath = "/html/body/div[1]/div/div[1]/div/button[2]"
-    cookiesElem = browser.find_element_by_xpath(cookiesXpath)
-    cookiesElem.click()
-
-def getWinningPostCode():
-    global winnerPostcode
-    winningTextXpath = "/html/body/div[2]/div[2]/div/main/div/section/div/div[1]/p[2]"
-    winnerPostcodeText = browser.find_element_by_xpath(winningTextXpath)
-    winnerPostcode = winnerPostcodeText.text.split('\n')[0]
 
 def sendEmail():
     global winnerPostcode
@@ -57,15 +14,48 @@ def sendEmail():
     yag = yagmail.SMTP(emailFrom, keyring.get_password('gmail', emailFrom))
     yag.send(emailValue, emailSubject, emailBody)
 
+def make_request():
+    cookies = {
+        'PHPSESSID': '3666568cee093c3f6ba9d857c93bd318',
+        '_cmpQcif3pcsupported': '1',
+        '_fbp': 'fb.1.1589014512656.938149904',
+        '_hjid': '0d776cee-ae09-433b-aa99-0ca13a852545',
+        '_ga': 'GA1.2.1179976700.1589014513',
+        '_gid': 'GA1.2.1925405355.1589014513',
+        'crfgL0cSt0r': 'true',
+        'ntv_as_us_privacy': '1---',
+        '__gads': 'ID=98cdb65f4afd9ecc:T=1589014521:S=ALNI_MYQ8-9rdEzaiikx8Ji4bF0do-wahA',
+        'googlepersonalization': 'OzIF-5OzIGAwgA',
+        'eupubconsent': 'BOzIF-5OzIGAwAKAAAENAAAA-AAAAA',
+        'euconsent': 'BOzIF-5OzIGAwAKAACENDJ-AAAAvhr_7__7-_9_-_f__9uj3Gr_v_f__32ccL59v3h_7v-_7fi_-1nV4u_1vft9ydk1-5ctDztp507iakiPHmqNeb1n_mz1eZpRP58k09j5337Ew_v8_v-b7BCPN9Y3v-8K4',
+        'FPL-SplitTest': 'v2-without-trustpilot',
+        'fplCdn': 'useCdn',
+        'FPL-LoggedIn': '2647436LoggedInPdijOeJ6g7xWFoks5hyUN2VTEn8YMu1fpclzQS0wKLB4GvrDCXqbRtmAIH3aZ9%3BLoggedIn%3B1603967283%3B3efc34f2aa477c22c4aff58e0a7f0112258e6746',
+        'hotjar': 'norecord',
+    }
+
+    headers = {
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://pickmypostcode.com/?action=login-signin',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    params = (
+        ('_', '1589103153431'),
+    )
+
+    response = requests.get('https://pickmypostcode.com/api/index.php/draws/main/', headers=headers, params=params, cookies=cookies)
+    json = json.loads(response.text)
+    winnerPostcode = json[0]
 
 def start():
-    loadBrowser()
-    time.sleep(1)
-    enterDetails()
-    goToHomePage()
-    time.sleep(1)
-    acceptCookies()
-    getWinningPostCode()
+    make_request()
     sendEmail()
 
 def check_postcode():
