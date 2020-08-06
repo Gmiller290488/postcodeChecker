2a4d4e2a7a10575b939863392a4c1d4f923c05cc
CTingCTer <gmiller290488@gmail.com>
2020-08-06 15:49:43 +0100

    Updated the email winner file

--- postCodeBE/postCodeBE/settings.py
+++ postCodeBE/postCodeBE/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'postcodeApp.apps.PostcodeappConfig',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
--- postCodeBE/postcodeApp/apps.py
+++ postCodeBE/postcodeApp/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class PostcodeappConfig(AppConfig):
     name = 'postcodeApp'
